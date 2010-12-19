b901e0c5ec40a79e27819f4c7e4cb67e337166c4
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-12-19 01:03:14 -0200

    fixed bug search and add makefile

--- lib/WWW/Orkut/API/Search.pm
+++ lib/WWW/Orkut/API/Search.pm
@@ -414,3 +414,89 @@ sub busca_tpc_parser {
 __PACKAGE__->meta->make_immutable;
 1;
 __END__
+
+=head1 NAME
+
+WWW::Orkut::API::Search - The great new WWW::Orkut::API::Search!
+
+=head1 VERSION
+
+Version 0.01
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WWW::Orkut::API::Search;
+
+    my $foo = WWW::Orkut::API::Search->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 AUTHOR
+
+Daniel de Oliveira Mantovani, C<< <daniel.oliveira.mantovani at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-orkut-api-search at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Orkut-API-Search>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::Orkut::API::Search
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Orkut-API-Search>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-Orkut-API-Search>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-Orkut-API-Search>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-Orkut-API-Search/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Daniel de Oliveira Mantovani.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of WWW::Orkut::API::Search
